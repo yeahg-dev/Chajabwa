4371728d72854a8210cc970d1ef10cda8c9e9655
yeahg-dev <yeahg.dev@gmail.com>
2022-08-12 23:08:56 +0900

    ✨FEAT: AppCategory 열거형 정의 #1

---
+++ app-show-room/app-show-room/Model/AppCategory.swift
@@ -0,0 +1,39 @@
+//
+//  AppCategory.swift
+//  app-show-room
+//
+//  Created by Moon Yeji on 2022/08/12.
+//
+
+import Foundation
+
+enum AppCategory: String, Codable {
+    
+    case books = "Books"
+    case business = "Business"
+    case developerTools = "Developer Tools"
+    case education =  "Education"
+    case entertainment = "Entertainment"
+    case finance = "Finance"
+    case foodAndDrink = "Food & Drink"
+    case games = "Games"
+    case graphicsAndDesign = "Graphics & Design"
+    case healthAndFitness = "Health & Fitness"
+    case lifeStyle = "Life Style"
+    case kids = "Kids"
+    case magazinesAndNewspapers = "Magazines and Newspaper"
+    case medical = "Medical"
+    case music = "Music"
+    case navigation = "Navigation"
+    case news = "News"
+    case photoAndVideo = "Photo & Video"
+    case productivity = "Productivity"
+    case reference = "Reference"
+    case safariExtension
+    case shopping
+    case socialNetworking = "Social Networking"
+    case sports = "Sports"
+    case travel = "Travel"
+    case utilities = "Utilities"
+    case weahter = "Weather"
+}
