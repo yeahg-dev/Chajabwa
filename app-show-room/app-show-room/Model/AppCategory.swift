69414406925f51a3371fd65f1601aea81fb3dfd2
yeahg-dev <yeahg.dev@gmail.com>
2022-08-12 23:44:07 +0900

    ✨FEAT: AppCategory의 image 프로퍼티 구현 #1

--- app-show-room/app-show-room/Model/AppCategory.swift
+++ app-show-room/app-show-room/Model/AppCategory.swift
@@ -5,7 +5,7 @@
 //  Created by Moon Yeji on 2022/08/12.
 //
 
-import Foundation
+import UIKit
 
 enum AppCategory: String, Codable {
     
@@ -36,4 +36,64 @@ enum AppCategory: String, Codable {
     case travel = "Travel"
     case utilities = "Utilities"
     case weahter = "Weather"
+    
+    var image: UIImage? {
+        switch self {
+        case .books:
+            return UIImage(systemName: "books.vertical.fill")
+        case .business:
+            return UIImage(systemName: "suitcase.fill")
+        case .developerTools:
+            return UIImage(systemName: "hammer.fill")
+        case .education:
+            return UIImage(systemName: "graduationcap.fill")
+        case .entertainment:
+            return UIImage(systemName: "face.smiling.fill")
+        case .finance:
+            return UIImage(systemName: "banknote.fill")
+        case .foodAndDrink:
+            return UIImage(systemName: "fork.knife")
+        case .games:
+            return UIImage(systemName: "gamecontroller.fill")
+        case .graphicsAndDesign:
+            return UIImage(systemName: "lasso.and.sparkles")
+        case .healthAndFitness:
+            return UIImage(systemName: "bicycle")
+        case .lifeStyle:
+            return UIImage(systemName: "house.fill")
+        case .kids:
+            return UIImage(systemName: "face.dashed.fill")
+        case .magazinesAndNewspapers:
+            return UIImage(systemName: "magazine.fill")
+        case .medical:
+            return UIImage(systemName: "staroflife.circle.fill")
+        case .music:
+            return UIImage(systemName: "music.quarternote.3")
+        case .navigation:
+            return UIImage(systemName: "location.circle.fill")
+        case .news:
+            return UIImage(systemName: "antenna.radiowaves.left.and.right")
+        case .photoAndVideo:
+            return UIImage(systemName: "camera.fill")
+        case .productivity:
+            return UIImage(systemName: "paperplane.fill")
+        case .reference:
+            return UIImage(systemName: "magnifyingglass")
+        case .safariExtension:
+            return UIImage(systemName: "safari.fill")
+        case .shopping:
+            return UIImage(systemName: "cart.fill")
+        case .socialNetworking:
+            return UIImage(systemName: "message.fill")
+        case .sports:
+            return UIImage(systemName: "sportscourt.fill")
+        case .travel:
+            return UIImage(systemName: "binoculars.fill")
+        case .utilities:
+            return UIImage(systemName: "calendar.badge.clock")
+        case .weahter:
+            return UIImage(systemName: "cloud.sun.fill")
+        }
+    }
+    
 }
