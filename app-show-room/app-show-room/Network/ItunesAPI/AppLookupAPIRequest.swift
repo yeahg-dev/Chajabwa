3f7e347cfa6bfa6ffcc55bb2921f5da9a1fc6547
yeahg-dev <yeahg.dev@gmail.com>
2022-08-13 21:26:42 +0900

    ✨FEAT: AppLookupResponse, AppResponse 타입 정의 #2

--- app-show-room/app-show-room/Network/ItunesAPI/AppLookupAPIRequest.swift
+++ app-show-room/app-show-room/Network/ItunesAPI/AppLookupAPIRequest.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct AppLookupAPIRequest: iTunesAPIRequest {
 
-    typealias APIResponse = AppResponse
+    typealias APIResponse = AppLookupResponse
     
     var httpMethod: HTTPMethod  = .get
     var path: String = "/lookup"
@@ -19,9 +19,67 @@ struct AppLookupAPIRequest: iTunesAPIRequest {
     init(appID: Int) {
         self.query = ["id": appID]
     }
-}
-
-struct AppResponse: Decodable {
     
 }
 
+struct AppLookupResponse: Decodable {
+    
+    let resultCount: Int
+    let results: [AppResponse]
+    
+}
+
+struct AppResponse {
+    
+    let screenshotUrls: [String]
+    let ipadScreenshotUrls: [String]
+    let appletvScreenshotUrls: [String]
+    let artworkUrl60, artworkUrl512, artworkUrl100: String
+    let artistViewURL: String
+    let features: [String]
+    let isGameCenterEnabled: Bool
+    let supportedDevices: [String]
+    let advisories: [String]
+    let kind: String
+    let averageUserRating: Double
+    let releaseNotes, minimumOSVersion, trackCensoredName: String
+    let languageCodesISO2A: [String]
+    let fileSizeBytes, formattedPrice, contentAdvisoryRating: String
+    let averageUserRatingForCurrentVersion: Double
+    let userRatingCountForCurrentVersion: Int
+    let trackViewURL: String
+    let trackContentRating, resultDescription: String
+    let releaseDate: Date
+    let bundleID, sellerName, currency, primaryGenreName: String
+    let primaryGenreID: Int
+    let currentVersionReleaseDate: Date
+    let isVppDeviceBasedLicensingEnabled: Bool
+    let genreIDS: [String]
+    let version, wrapperType: String
+    let artistID: Int
+    let artistName: String
+    let genres: [String]
+    let price, trackID: Int
+    let trackName: String
+    let userRatingCount: Int
+
+}
+
+extension AppResponse: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case screenshotUrls, ipadScreenshotUrls, appletvScreenshotUrls, artworkUrl60, artworkUrl512, artworkUrl100, features, isGameCenterEnabled, supportedDevices, advisories, kind, averageUserRating, releaseNotes, trackCensoredName, languageCodesISO2A, fileSizeBytes, formattedPrice, contentAdvisoryRating, averageUserRatingForCurrentVersion, userRatingCountForCurrentVersion, trackContentRating, releaseDate, sellerName, currency, primaryGenreName,
+             currentVersionReleaseDate, isVppDeviceBasedLicensingEnabled, version, wrapperType,
+             artistName, genres, price, trackName, userRatingCount
+        case artistViewURL = "artistViewUrl"
+        case minimumOSVersion = "minimumOsVersion"
+        case trackViewURL = "trackViewUrl"
+        case resultDescription = "description"
+        case bundleID = "bundleId"
+        case primaryGenreID = "primaryGenreId"
+        case genreIDS = "genreIds"
+        case artistID = "artistId"
+        case trackID = "trackId"
+
+    }
+}
