15442be60581bbe2f7d2924cdb3b73d7174252b9
yeahg-dev <yeahg.dev@gmail.com>
2022-08-15 02:02:13 +0900

    ✨FEAT: Observable 타입 구현

---
+++ app-show-room/app-show-room/Utility/Observable.swift
@@ -0,0 +1,44 @@
+//
+//  Observable.swift
+//  app-show-room
+//
+//  Created by Moon Yeji on 2022/08/15.
+//
+
+import Foundation
+
+final class Observable<Value> {
+    
+    struct Observer<Value> {
+        weak var observer: AnyObject?
+        let valueDidSet: (Value) -> Void
+    }
+    
+    private var observers = [Observer<Value>]()
+    
+    var value: Value {
+        didSet { notifyObserversOnMainThread() }
+    }
+    
+    init(_ value: Value) {
+        self.value = value
+    }
+    
+    func observe(
+        on observer: AnyObject,
+        subscribeBlock: @escaping (Value) -> Void) {
+        observers.append(Observer(observer: observer, valueDidSet: subscribeBlock))
+        subscribeBlock(self.value)
+    }
+    
+    func remove(observer: AnyObject) {
+        observers = observers.filter { $0.observer !== observer }
+    }
+    
+    private func notifyObserversOnMainThread() {
+        for observer in observers {
+            DispatchQueue.main.async { observer.valueDidSet(self.value) }
+        }
+    }
+    
+}
