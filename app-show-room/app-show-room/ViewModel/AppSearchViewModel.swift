d1f40365b165e4f1f1340e7f450ba7c5803660d6
yeahg-dev <yeahg.dev@gmail.com>
2022-08-15 15:06:07 +0900

    ✨FEAT: AppSearchViewModel 구현 #5

--- app-show-room/app-show-room/ViewModel/AppSearchViewModel.swift
+++ app-show-room/app-show-room/ViewModel/AppSearchViewModel.swift
@@ -1,8 +1,67 @@
 //
-//  AppSearchViewModle.swift
+//  AppSearchViewModel.swift
 //  app-show-room
 //
 //  Created by Moon Yeji on 2022/08/15.
 //
 
 import Foundation
+
+// MARK: - AppSearchViewModelInput
+
+protocol AppSearchViewModelInput {
+    
+    func didTappedSearch(with input: String)
+}
+
+// MARK: - AppSearchViewModelOutput
+
+protocol AppSearchViewModelOutput {
+    
+    var searchBarPlaceholder: Observable<String> { get }
+    var searchResult: Observable<AppDetail?> { get }
+    var searchFailureAlert: Observable<AlertText?> { get }
+}
+
+// MARK: - AppSearchViewModel
+
+struct AppSearchViewModel: AppSearchViewModelOutput {
+
+    private let appSearchUsecase: AppSearchUsecase
+    
+    init(appSearchUsecase: AppSearchUsecase) {
+        self.appSearchUsecase = appSearchUsecase
+    }
+    
+    // MARK: - Output
+    var searchBarPlaceholder = Observable(AppSerachSceneText().title)
+    var searchResult = Observable<AppDetail?>(.none)
+    var searchFailureAlert = Observable<AlertText?>(.none)
+    
+}
+
+// MARK: - Input
+
+extension AppSearchViewModel: AppSearchViewModelInput {
+    
+    func didTappedSearch(with input: String) {
+        self.appSearchUsecase.searchApp(
+            of: input) { result in
+                switch result {
+                case .success(let appDetail):
+                    self.searchResult.value = appDetail
+                case.failure(let error):
+                    self.handleSearchError(error)
+                }
+            }
+    }
+    
+    private func handleSearchError(_ error: Error) {
+        if let appSearchUseacseError = error as? AppSearchUsecaseError,
+           appSearchUseacseError == AppSearchUsecaseError.invalidInputType {
+            self.searchFailureAlert.value = AppSerachSceneText.FailureAlertText()
+        }
+        // TODO: - 검색 실패에 대한 오류 메시지 전달
+    }
+    
+}
