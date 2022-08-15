eeabe162f2d3ddf2bf0a9d6fb69dfb2e088ce833
yeahg-dev <yeahg.dev@gmail.com>
2022-08-16 00:57:43 +0900

    ✨FEAT: url로 UIImageView 설정 함수, ImageCache 구현

---
+++ app-show-room/app-show-room/Utility/Extension/UIImageView+Extension.swift
@@ -0,0 +1,56 @@
+//
+//  UIImageView+Extension.swift
+//  app-show-room
+//
+//  Created by Moon Yeji on 2022/08/16.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    func setImage(with url: URL, defaultImage: UIImage) -> CancellableTask? {
+        let imageCache = ImageCache()
+        let cacheKey = url.absoluteString
+        
+        if let cachedImage = imageCache.getImage(of: cacheKey) {
+            self.image = cachedImage
+            return nil
+        }
+   
+        let task = URLSession.shared.dataTask(with: url) {
+            [weak self] data, _, error in
+            if let _ = error {
+                DispatchQueue.main.async {
+                    self?.image = defaultImage
+                }
+                return
+            } else {
+                DispatchQueue.main.async {
+                    guard let imageData = data,
+                          let image = UIImage(data: imageData) else {
+                        return }
+                    self?.image = image
+                    imageCache.cache(image, of: cacheKey)
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+}
+
+protocol CancellableTask {
+    
+    func cancelTask()
+}
+
+extension URLSessionDataTask: CancellableTask {
+    
+    func cancelTask() {
+        self.cancel()
+    }
+    
+}
