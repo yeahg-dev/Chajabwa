16efddd8c791404733843abcb10102830fd45312
yeahg-dev <yeahg.dev@gmail.com>
2022-09-19 20:52:15 +0900

    ✨REFACTOR: SupplementaryView 타입 분리, 레이아웃 수정

--- app-show-room/app-show-room/Scene/AppDetail/Cell/PaddingTitleSupplementaryView.swift
+++ app-show-room/app-show-room/Scene/AppDetail/Cell/PaddingTitleSupplementaryView.swift
@@ -1,31 +1,36 @@
 //
-//  TitleSupplementaryView.swift
+//  PaddingTitleSupplementaryView.swift
 //  app-show-room
 //
-//  Created by Moon Yeji on 2022/09/05.
+//  Created by Moon Yeji on 2022/09/13.
 //
 
 import UIKit
 
 private enum Design {
-
+    
+    static let paddingLeading: CGFloat = 25
     static let paddingTop: CGFloat = 15
+    static let paddingTrailing: CGFloat = 25
     
     static let separatorWidth: CGFloat = 0.5
     static let separatorColor: CGColor = UIColor.systemGray3.cgColor
     
     static let titleFontSize: CGFloat = 22
+   
 }
 
-// MARK: - TitleSupplementaryView
+// MARK: - PaddingTitleSupplementaryView
+// inset을 적용하지 못하는 Section을 위한 TitleSupplementaryView
+// leading, trailing에 padding을 적용함으로써 inset을 적용한 것과 같은 효과를 줌
 
-final class TitleSupplementaryView: UICollectionReusableView {
-    
+final class PaddingTitleSupplementaryView: UICollectionReusableView {
+  
     private let titleLabel = UILabel()
     lazy var separatorLayer: CALayer = {
         let layer = CALayer()
-        let origin = CGPoint(x: 0 ,y: 0)
-        let size = CGSize(width: self.bounds.width, height: Design.separatorWidth)
+        let origin = CGPoint(x: Design.paddingLeading ,y: 0)
+        let size = CGSize(width: self.bounds.width - (Design.paddingLeading + Design.paddingTrailing), height: Design.separatorWidth)
         layer.frame = CGRect(origin: origin, size: size)
         layer.backgroundColor = Design.separatorColor
         layer.drawsAsynchronously = true
@@ -45,19 +50,17 @@ final class TitleSupplementaryView: UICollectionReusableView {
     func bind(title: String?) {
         titleLabel.text = title
     }
-    
+   
     private func configureSubview() {
         addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.adjustsFontForContentSizeCategory = true
         NSLayoutConstraint.activate([
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Design.paddingLeading),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Design.paddingTrailing),
             titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: Design.paddingTop),
             titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
         titleLabel.font = .boldSystemFont(ofSize: Design.titleFontSize)
     }
-    
 }
-
