0ac378630f1346d2a88c6cce7fbfecf8bb4c94f2
yeahg-dev <yeahg.dev@gmail.com>
2022-12-30 16:09:09 +0900

    ✨FEAT: RecentSearchKeyword 타입 정의 #17

---
+++ app-show-room/app-show-room/Domain/Entity/AppSearch/RecentSearchKeyword.swift
@@ -0,0 +1,16 @@
+//
+//  RecentSearchKeyword.swift
+//  app-show-room
+//
+//  Created by Moon Yeji on 2022/12/29.
+//
+
+import Foundation
+
+struct RecentSearchKeyword {
+    
+    let identifier = UUID()
+    let keyword: String
+    let date: Date
+    
+}
