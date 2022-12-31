33c0703d022c3db6258fd872215f77b3e6d2ba15
yeahg-dev <yeahg.dev@gmail.com>
2022-12-31 16:43:23 +0900

    ✨FEAT: SearckKeywordRealmStore 구현 #17

---
+++ app-show-room/app-show-room/Database/Realm/SearckKeywordRealmStore.swift
@@ -0,0 +1,25 @@
+//
+//  SearckKeywordRealmStore.swift
+//  app-show-room
+//
+//  Created by Moon Yeji on 2022/12/31.
+//
+
+import Foundation
+
+import RealmSwift
+
+final class SearckKeywordRealmStore {
+    
+    private let defaultRealm: Realm!
+    
+    init?() {
+        do {
+            defaultRealm = try Realm()
+        } catch  {
+            print("Error initiating new realm \(error)")
+            return nil
+        }
+    }
+    
+}
