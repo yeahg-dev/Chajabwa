62376043aa5213c8a842f59cc3a0eac3ece2009b
yeahg-dev <yeahg.dev@gmail.com>
2023-01-01 17:18:23 +0900

    ✨FEAT: AppSearchUsecase에 RecentSearchKeyword 저장 및 검색 기능 추가 #17

--- app-show-room/app-show-room/Usecase/AppSearchUsecase.swift
+++ app-show-room/app-show-room/Usecase/AppSearchUsecase.swift
@@ -10,28 +10,76 @@ import Foundation
 struct AppSearchUsecase {
     
     private let appDetailRepository: AppDetailRepository
+    private let searchKeywordRepository: SearchKeywordRepository
     
-    init(appDetailRepository: AppDetailRepository = ItunesAppDetailRepository()) {
+    init(
+        appDetailRepository: AppDetailRepository = ItunesAppDetailRepository(),
+        searchKeywordRepository: SearchKeywordRepository
+    ) {
         self.appDetailRepository = appDetailRepository
+        self.searchKeywordRepository = searchKeywordRepository
     }
     
-    func searchAppDetail(of input: String) async throws -> [AppDetail] {
-        let country = AppSearchingConfiguration.countryISOCode.isoCode
-        let softwareType = AppSearchingConfiguration.softwareType.rawValue
-        
+    func searchAppDetail(
+        of searchKeyword: RecentSearchKeyword)
+    async throws -> [AppDetail]
+    {
+        let input = searchKeyword.keyword
+        let configuration = searchKeyword.configuration
         if let id = Int(input) {
             let appDetail = try await self.appDetailRepository.fetchAppDetail(
                 of: id,
-                country: country,
-                software: softwareType)
+                country: configuration.country.name,
+                software: configuration.softwareType.rawValue)
             return [appDetail]
         } else {
             let appDetails = try await self.appDetailRepository.fetchAppDetails(
                 of: input,
-                country: country,
-                software: softwareType)
+                country: configuration.country.name,
+                software: configuration.softwareType.rawValue)
             return appDetails
         }
     }
- 
+    
+    func searchAppDetail(of input: String) async throws -> [AppDetail] {
+        let currentCountry = AppSearchingConfiguration.countryISOCode.isoCode
+        let currentSoftwareType = AppSearchingConfiguration.softwareType.rawValue
+        
+        createRecentSearchKeyword(with: input)
+        if let id = Int(input) {
+            let appDetail = try await self.appDetailRepository.fetchAppDetail(
+                of: id,
+                country: currentCountry,
+                software: currentSoftwareType)
+            return [appDetail]
+        } else {
+            let appDetails = try await self.appDetailRepository.fetchAppDetails(
+                of: input,
+                country: currentCountry,
+                software: currentSoftwareType)
+            return appDetails
+        }
+        
+    }
+    
+    private func createRecentSearchKeyword(with input: String) {
+        guard !input.isEmpty else {
+            return
+        }
+        
+        do {
+            try searchKeywordRepository.create(
+                keyword:
+                    RecentSearchKeyword(
+                        keyword: input,
+                        date: Date(),
+                        configuration: SearchConfiguration(
+                            country: AppSearchingConfiguration.countryISOCode,
+                            softwareType: AppSearchingConfiguration.softwareType) )
+            )
+        } catch {
+            print("Failed to create RecentSearchKeyword. error :\(error)")
+        }
+    }
+    
 }
