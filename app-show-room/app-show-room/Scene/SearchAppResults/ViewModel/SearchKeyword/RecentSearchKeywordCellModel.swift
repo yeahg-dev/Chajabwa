5c8ded113573d2d5686e41ce2609c77401890646
yeahg-dev <yeahg.dev@gmail.com>
2023-01-04 18:53:03 +0900

    ✨FEAT: SearchKeywordTableViewCell dateformat 수정 #17

--- app-show-room/app-show-room/Scene/SearchAppResults/ViewModel/SearchKeyword/RecentSearchKeywordCellModel.swift
+++ app-show-room/app-show-room/Scene/SearchAppResults/ViewModel/SearchKeyword/RecentSearchKeywordCellModel.swift
@@ -9,6 +9,12 @@ import UIKit
 
 struct RecentSearchKeywordCellModel {
     
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM.dd."
+        return formatter
+    }()
+    
     let keyword: String
     let date: String
     let countryFlag: String
@@ -16,7 +22,7 @@ struct RecentSearchKeywordCellModel {
     
     init(keyword: RecentSearchKeyword) {
         self.keyword = keyword.keyword
-        self.date = keyword.date.formatted(date: .numeric, time: .omitted)
+        self.date = RecentSearchKeywordCellModel.dateFormatter.string(from: keyword.date)
         self.countryFlag = keyword.configuration.country.flag
         self.softwareIcon = keyword.configuration.softwareType.iconImage
     }
