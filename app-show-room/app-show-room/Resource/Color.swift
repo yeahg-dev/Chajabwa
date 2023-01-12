2bc4592a4962ab0382831ffb2546b40aee872b0b
yeahg-dev <yeahg.dev@gmail.com>
2023-01-12 21:54:14 +0900

    ✨FEAT: Asset Color 기반으로 Color 타입 구현 #19

---
+++ app-show-room/app-show-room/Resource/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  app-show-room
+//
+//  Created by Moon Yeji on 2023/01/12.
+//
+
+import UIKit
+
+enum Color {
+
+    static let blueGreen = UIColor(named: "blueGreen")!
+    static let skyBlue = UIColor(named: "skyBlue")!
+    static let grayLavender = UIColor(named: "grayLavender")!
+    static let mauveLavender = UIColor(named: "mauveLavender")!
+    static let shadeLavender = UIColor(named: "shadeLavender")!
+    static let favoriteLavender = UIColor(named: "favoriteLavender")!
+    static let lightGray = UIColor(named: "lightGray")!
+    
+}
