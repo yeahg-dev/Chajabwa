23333860a4794130254b2d51f6aa4fb8d6a35c9a
yeahg-dev <yeahg.dev@gmail.com>
2023-02-13 21:18:30 +0900

    👍REFACTOR: APIService 공통 사용 메서드 프로토콜 기본구현으로 이동 #25

--- app-show-room/app-show-room/Domain/Service/ItunesAPI/iTunesAPIService.swift
+++ app-show-room/app-show-room/Domain/Service/ItunesAPI/iTunesAPIService.swift
@@ -16,47 +16,6 @@ struct iTunesAPIService: APIService {
         self.session = session
     }
     
-    func execute<T: APIRequest>(request: T) async throws -> T.APIResponse {
-        guard let urlRequest = request.urlRequest else {
-            throw APIError.invalidURL
-        }
-        
-        let (data, response) = try await session.data(for: urlRequest)
-        guard let response = response as? HTTPURLResponse,
-              (200...299).contains(response.statusCode) else {
-            print("\(T.self) failed to receive success response(status code: 200-299)")
-            throw APIError.HTTPResponseFailure
-        }
-        
-        guard let parsedData: T.APIResponse = parse(response: data) else {
-            print("parsing failed. type: \(T.APIResponse.Type.self) ")
-            throw APIError.invalidParsedData
-        }
-        
-        return parsedData
-    }
-    
-    func getResponse<T: APIRequest>(
-        request: T)
-    -> AnyPublisher<T.APIResponse, Error>
-    {
-        guard let urlRequest = request.urlRequest else {
-            return Result.Publisher(.failure(APIError.invalidURL))
-                .eraseToAnyPublisher()
-        }
-       
-        return URLSession.shared.dataTaskPublisher(for: urlRequest)
-            .tryMap { (data, response) -> Data in
-                guard let httpResponse = response as? HTTPURLResponse,
-                      (200...299).contains(httpResponse.statusCode) else {
-                    throw APIError.HTTPResponseFailure
-                }
-                return data
-            }
-            .decode(type: T.APIResponse.self, decoder: JSONDecoder())
-            .eraseToAnyPublisher()
-    }
-    
 }
 
 extension iTunesAPIService {
