00f221e9cf28a3b7fddde180ebad0c5cbc30deb8
yeahg-dev <yeahg.dev@gmail.com>
2023-03-17 22:50:01 +0900

    🗂FILE: Service 폴더 상위 디렉토리로 변경

---
+++ app-show-room/app-show-room/Service/OpenAPI/CountryCodeService/Request/CountryCodeListRequest.swift
@@ -0,0 +1,36 @@
+//
+//  CountryCodeListRequest.swift
+//  app-show-room
+//
+//  Created by Moon Yeji on 2023/02/13.
+//
+
+import Foundation
+
+struct CountryCodeListRequest: APIRequest {
+    
+    typealias APIResponse = CountryCodeList
+    
+    var httpMethod: HTTPMethod  = .get
+    var baseURLString: String = "http://apis.data.go.kr/1262000/CountryCodeService2"
+    var path: String = "/getCountryCodeList2"
+    var query: [String: Any]
+    var body: Data? = nil
+    var header: [String: String] = [:]
+    
+    var url: URL? {
+        var urlComponents = URLComponents(string: baseURLString + path)
+        urlComponents?.queryItems = query.map {
+            URLQueryItem(name: $0.key, value: "\($0.value)") }
+        let encodedQuery = urlComponents?.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+        urlComponents?.percentEncodedQuery = encodedQuery
+        return urlComponents?.url
+    }
+    
+    init(pageNo: Int, numOfRows: Int) {
+        self.query = ["serviceKey": Bundle.main.countryCodeAPIKey,
+                      "numOfRows": numOfRows,
+                      "pageNo": pageNo]
+    }
+
+}
