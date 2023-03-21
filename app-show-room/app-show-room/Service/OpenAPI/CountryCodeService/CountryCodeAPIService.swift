af5bbe24571a465679f192c24a48108139d181f7
yeahg-dev <yeahg.dev@gmail.com>
2023-03-21 23:12:05 +0900

    👍REFACTOR: CountryCodeAPIService URLSessionDownloadDelegate 구현 #31
    - 다운로드 progress를 얻기 위해 URLSessionDownloadDelegate로 api call 구현

--- app-show-room/app-show-room/Service/OpenAPI/CountryCodeService/CountryCodeAPIService.swift
+++ app-show-room/app-show-room/Service/OpenAPI/CountryCodeService/CountryCodeAPIService.swift
@@ -7,41 +7,64 @@
 
 import Foundation
 
-struct CountryCodeAPIService: APIService {
+final class CountryCodeAPIService: NSObject {
     
-    var session: URLSession = URLSession(configuration: .default)
+    private lazy var session = URLSession(
+        configuration: .default,
+        delegate: self,
+        delegateQueue: nil)
     
-    func requestAllCountryCode() async throws -> [CountryCode] {
-        let request = CountryCodeListRequest(pageNo: 1, numOfRows: 240)
-        let countryCodeList: CountryCodeList = try await getResponse(request: request)
-        return countryCodeList.data
+    func fetchCountryCodes() {
+        guard let request = CountryCodeListRequest(pageNo: 1, numOfRows: 240).urlRequest else {
+            // Noti with error
+            return
+        }
+        let downloadTask = session.downloadTask(with: request)
+        downloadTask.resume()
+        // Noti with progress
+    }
+ 
+}
+
+extension CountryCodeAPIService: URLSessionDownloadDelegate, URLSessionDelegate {
+    
+    func urlSession(
+        _ session: URLSession,
+        downloadTask: URLSessionDownloadTask,
+        didFinishDownloadingTo location: URL)
+    {
+        guard let httpResponse = downloadTask.response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else {
+            print("\(self) : failed to receive success response(status code: 200-299)")
+            // Noti with error
+            return
+        }
+        
+        guard let contentType = httpResponse.value(forHTTPHeaderField: "Content-Type"),
+        contentType == "application/json;charset=UTF-8" else {
+            print("\(self) : response Content-Type is \(String(describing: httpResponse.value(forHTTPHeaderField: "Content-Type")))")
+            // Noti with error
+            return
+        }
+
+        guard let data = try? Data(contentsOf: location) else {
+            print("Type[\(self)] data를 읽을 수 없음")
+            // Noti with error
+            return
+        }
+        
+        guard let parsedData = try? JSONDecoder().decode(CountryCodeList.self, from: data) else {
+            print("parsing failed. type: \(CountryCodeList.self) ")
+            // Noti with error
+            return
+        }
+        
+        appendCountries(countryCodeList: parsedData)
+        // Noti with success
     }
     
-    func getResponse<T: APIRequest>(request: T) async throws -> T.APIResponse {
-        guard let urlRequest = request.urlRequest else {
-            throw APIError.invalidURL
-        }
-        
-        let (data, response) = try await session.data(for: urlRequest)
-        
-        guard let response = response as? HTTPURLResponse,
-              (200...299).contains(response.statusCode) else {
-            print("\(T.self) failed to receive success response(status code: 200-299)")
-            throw APIError.HTTPResponseFailure
-        }
-        
-        guard let contentType = response.value(forHTTPHeaderField: "Content-Type"),
-        contentType == "application/json;charset=UTF-8" else {
-            print("\(T.self) : response Content-Type is \(String(describing: response.value(forHTTPHeaderField: "Content-Type")))")
-            throw APIError.HTTPResponseFailure
-        }
-        
-        guard let parsedData: T.APIResponse = parse(response: data) else {
-            print("parsing failed. type: \(T.APIResponse.Type.self) ")
-            throw APIError.invalidParsedData
-        }
-        
-        return parsedData
+    private func appendCountries(countryCodeList: CountryCodeList) {
+        Country.list += countryCodeList.data.compactMap {$0.toDomain()}
     }
     
 }
